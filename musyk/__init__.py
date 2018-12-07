979e29cb09b62b1a849e04760cbfa71f05a22653
kalyanteja <kalyansmacks@gmail.com>
2018-12-07 13:25:53 +0530

    added postgresql stuff

--- musyk/__init__.py
+++ musyk/__init__.py
@@ -5,8 +5,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qkjezzxf:2-_lrfVe4RFuOGFpl_VdFqcy4UfWR3WN@manny.db.elephantsql.com:5432/qkjezzxf'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qkjezzxf:2-_lrfVe4RFuOGFpl_VdFqcy4UfWR3WN@manny.db.elephantsql.com:5432/qkjezzxf'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
