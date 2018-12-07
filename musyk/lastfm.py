dca7489582941195e649bbbf032cb6dc986d0335
kalyanteja <kalyansmacks@gmail.com>
2018-12-07 13:58:27 +0530

    string format to support gcloud

--- musyk/lastfm.py
+++ musyk/lastfm.py
@@ -2,7 +2,7 @@ import requests
 
 # add a last fm key
 LASTFM_API_KEY = '12d5ddb92cc3bfaefd9d016e488d0cd8'
-LASTFM_TOPTRACKS_API_URL = f'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key={LASTFM_API_KEY}&format=json'
+LASTFM_TOPTRACKS_API_URL = 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key={}&format=json'
 LASTFM_TOPTRACKS_BY_COUNTRY_API_URL = ('http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country={}&api_key={}&format=json')
 
 def top_tracks(country):
@@ -10,7 +10,7 @@ def top_tracks(country):
         if country:
             data = requests.get(LASTFM_TOPTRACKS_BY_COUNTRY_API_URL.format(country, LASTFM_API_KEY)).json()
         else:
-            data = requests.get(LASTFM_TOPTRACKS_API_URL).json()
+            data = requests.get(LASTFM_TOPTRACKS_API_URL.format(LASTFM_API_KEY)).json()
     except Exception as exc:
         print(exc)
         data = None
